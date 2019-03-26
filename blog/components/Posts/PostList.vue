0632007e93e605912795dd2def942e41e243d56a
Harry beckwith <harry.beckwith@itech.media>
2019-03-26 19:40:36 +0000

    starting layout created

---
+++ blog/components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      thumbnail="http://www.bpifrance.fr/var/bpifrance/storage/images/bpifrance/a-la-une/actualites/innovation-la-deep-tech-promet-des-changements-profonds-35589/712694-1-fre-FR/Innovation-la-deep-tech-promet-des-changements-profonds.png"
+      title="hello"
+      previewText="first post"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="http://www.bpifrance.fr/var/bpifrance/storage/images/bpifrance/a-la-une/actualites/innovation-la-deep-tech-promet-des-changements-profonds-35589/712694-1-fre-FR/Innovation-la-deep-tech-promet-des-changements-profonds.png"
+      title="hello"
+      previewText="second post"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="http://www.bpifrance.fr/var/bpifrance/storage/images/bpifrance/a-la-une/actualites/innovation-la-deep-tech-promet-des-changements-profonds-35589/712694-1-fre-FR/Innovation-la-deep-tech-promet-des-changements-profonds.png"
+      title="hello"
+      previewText="third post"
+    />
+  </section>
+</template>
+
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ blog/pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about-page">
+    <p>about</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.about-page {
+  background-color: grey;
+}
+</style>
